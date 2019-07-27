2dd950f3fadd4c141682b132210df762e29996e0
abingpj <pj.abing@gmail.com>
2019-07-27 16:13:10 +0800

    Fix Products Page: Update the Assets Source Location

--- Pages/Product.php
+++ Pages/Product.php
@@ -13,8 +13,8 @@ include 'db.php';
 <head>
 	<meta charset="UTF-8">
 	<title>BF.POS</title>
-	<link rel="stylesheet"  href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet"  href="assets/css/custom.css">
+	<link rel="stylesheet"  href="../assets/css/bootstrap.min.css">
+	<link rel="stylesheet"  href="../assets/css/custom.css">
 
 
 
@@ -357,8 +357,8 @@ include 'db.php';
 
 </footer>
 
-<script src="assets/js/jquery-3.2.1.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="../assets/js/jquery-3.2.1.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
 
 </body>
 </html>
